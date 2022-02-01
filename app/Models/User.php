76e197efd4a201efb9c99072efb72190ca567dc5
vakobtu <v.gogatishvili23@gmail.com>
2022-02-02 02:36:50 +0400

    updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function statistics()
+    {
+    return $this->hasMany(Statistic::class, 'foreign_user_ID', 'id');
+    }
 }
